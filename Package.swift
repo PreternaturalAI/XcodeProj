29d4b632f7fe3a586f6e29c203d55412c04834ba
Vatsal Manot <vatsal.manot@yahoo.com>
2025-02-12 04:40:01 -0800

    Update package

--- Package.swift
+++ Package.swift
@@ -1,33 +1,39 @@
-// swift-tools-version:5.10.0
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "XcodeProj",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v16),
         .macOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6)
     ],
     products: [
-        .library(name: "XcodeProj", targets: ["XcodeProj"]),
+        .library(
+            name: "XcodeProj",
+            targets: ["XcodeProj"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.6.1")),
-        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.1")),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.1"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/vmanot/Swallow.git", branch: "master"),
     ],
     targets: [
-        .target(name: "XcodeProj",
-                dependencies: [
-                    .product(name: "PathKit", package: "PathKit"),
-                    .product(name: "AEXML", package: "AEXML"),
-                    "Swallow"
-                ],
-                swiftSettings: [
-                    .enableExperimentalFeature("StrictConcurrency"),
-                ]),
-        .testTarget(name: "XcodeProjTests", dependencies: ["XcodeProj"]),
+        .target(
+            name: "XcodeProj",
+            dependencies: [
+                .product(name: "PathKit", package: "PathKit"),
+                .product(name: "AEXML", package: "AEXML"),
+                "Swallow"
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+            ]
+        ),
+        .testTarget(
+            name: "XcodeProjTests",
+            dependencies: ["XcodeProj"]
+        ),
     ]
 )
